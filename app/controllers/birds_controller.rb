84c73d5ef7f853feef6a685835d7b63af7c1122b
lughjw <lughjw@gmail.com>
2020-03-16 13:59:04 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,8 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # The .to_json is being run implicitly because of render json:, but if we needed to we could force this to be json
+    # The 'render json:' is still important. When it's something nonsense like 'hopscotch:' it falls back to the default display in index.html.erb
+    render hopscotch: {birds: @birds, message_of_the_day: "Lets shoot them and make some pretty pictures"}.to_json
   end
 end
